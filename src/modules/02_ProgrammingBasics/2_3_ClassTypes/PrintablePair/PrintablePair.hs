5c4c2b73c8c5dc3990a44a1f313ae3bb0b772c76
eXetrum <shishimorovmaksim@gmail.com>
2023-11-10 17:47:48 +0300

    Update PrintablePair.hs

--- src/modules/02_ProgrammingBasics/2_3_ClassTypes/PrintablePair/PrintablePair.hs
+++ src/modules/02_ProgrammingBasics/2_3_ClassTypes/PrintablePair/PrintablePair.hs
@@ -1,10 +1,2 @@
-class Printable a where
-    toString :: a -> [Char]
-
-instance Printable Bool where
-    toString x 
-        | x == True = "true"
-        | x == False = "false"
-
-instance Printable () where
-    toString x = "unit type"
+instance (Printable a, Printable b) => Printable (a,b) where
+    toString (p1, p2) = "(" ++ (toString p1) ++ "," ++ (toString p2) ++ ")"

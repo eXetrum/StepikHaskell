23f0f6c480fcbcd1487cd0be5c32cbf2a0baa89a
eXetrum <shishimorovmaksim@gmail.com>
2023-11-12 02:30:04 +0300

    Update distance.hs

--- src/modules/04_Types/4_2_ProductTypesSumTypes/distance/distance.hs
+++ src/modules/04_Types/4_2_ProductTypesSumTypes/distance/distance.hs
@@ -7,4 +7,4 @@ distanceToOrigin :: Point -> Double
 distanceToOrigin (Point x y) = sqrt (x ^ 2 + y ^ 2)
 
 distance :: Point -> Point -> Double
-distance a b = distanceToOrigin $ b - a
+distance (Point x1 y1) (Point x2 y2) = sqrt $ (x1 - x2) ^ 2 + (y1 - y2) ^ 2

8015ab64f914942268a17d4255127d70b92dd849
eXetrum <shishimorovmaksim@gmail.com>
2023-11-18 19:49:35 +0300

    Update MaybeTree.hs

--- src/modules/05_Monads/5_1_Functor/MaybeTree/MaybeTree.hs
+++ src/modules/05_Monads/5_1_Functor/MaybeTree/MaybeTree.hs
@@ -1,5 +1,5 @@
+import Data.Functor
+
 instance Functor Tree where
-    fmap f (Leaf (Just x))              = Leaf (Just (f x))
-    fmap f (Leaf Nothing)               = Leaf Nothing
-    fmap f (Branch left (Just x) right) = Branch (fmap f left) (Just (f x)) (fmap f right)
-    fmap f (Branch left Nothing right)  = Branch (fmap f left) Nothing (fmap f right)
+    fmap f (Leaf x) = (Leaf $ f <$> x)
+    fmap f (Branch l x r) = Branch (f <$> l) (f <$> x) (f <$> r)

ecca19745f611cb1742ba0d8a179e0b20649e735
eXetrum <shishimorovmaksim@gmail.com>
2023-11-18 19:50:05 +0300

    Update MaybeTree.hs

--- src/modules/05_Monads/5_1_Functor/MaybeTree/MaybeTree.hs
+++ src/modules/05_Monads/5_1_Functor/MaybeTree/MaybeTree.hs
@@ -1,5 +1,5 @@
 import Data.Functor
 
 instance Functor Tree where
-    fmap f (Leaf x) = (Leaf $ f <$> x)
+    fmap f (Leaf x)       = (Leaf $ f <$> x)
     fmap f (Branch l x r) = Branch (f <$> l) (f <$> x) (f <$> r)

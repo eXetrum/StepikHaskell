69e0fcfcb3039e2b03021732a100de5a4148597b
eXetrum <shishimorovmaksim@gmail.com>
2023-11-21 18:21:03 +0300

    Update pythagoreanTriple.hs

--- src/modules/05_Monads/5_4_ListAndMaybeLikeMonad/pythagoreanTriple/pythagoreanTriple.hs
+++ src/modules/05_Monads/5_4_ListAndMaybeLikeMonad/pythagoreanTriple/pythagoreanTriple.hs
@@ -1,10 +1,2 @@
---Тип Board и функция nextPositions заданы, реализовывать их не нужно
-
-nextPositionsN :: Board -> Int -> (Board -> Bool) -> [Board]
-nextPositionsN b n pred
-  | n < 0 = []
-  | n == 0 = filter pred [b]
-  | otherwise =
-    do
-      positions <- nextPositions b
-      nextPositionsN positions (n - 1) pred
+pythagoreanTriple :: Int -> [(Int, Int, Int)]
+pythagoreanTriple x = do undefined

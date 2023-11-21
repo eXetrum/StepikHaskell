2120dc5c2cbfaaac88bb5e5f733cbc641b816f1a
eXetrum <shishimorovmaksim@gmail.com>
2023-11-21 18:29:19 +0300

    Update pythagoreanTriple.hs

--- src/modules/05_Monads/5_4_ListAndMaybeLikeMonad/pythagoreanTriple/pythagoreanTriple.hs
+++ src/modules/05_Monads/5_4_ListAndMaybeLikeMonad/pythagoreanTriple/pythagoreanTriple.hs
@@ -1,2 +1,7 @@
 pythagoreanTriple :: Int -> [(Int, Int, Int)]
-pythagoreanTriple x = do undefined
+pythagoreanTriple x = do
+  a <- [1..x]
+  b <- [1..x]
+  c <- [1..x]
+  if a ^ 2 + b ^ 2 == c ^ 2 && a < b && c <= x then "Z" else []
+  return (a, b, c)
